a22494a88f491c9f97abd3b96a8d019ba3844e07
MaksMusic <Maksmusic05@yandex.ru>
2022-09-05 23:29:15 +0300

    add Set List

---
+++ src/main/kotlin/Collection/Set.kt
@@ -0,0 +1,12 @@
+package Collection
+
+fun main() {
+    var setList = HashSet<String>()
+    setList.add("Joni")
+    setList.add("Joni")
+    setList.add("Joni")
+    setList.add("Joni")
+    setList.add("Joni")
+    setList.add("Joni")
+    println(setList)
+}
