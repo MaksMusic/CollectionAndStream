4008e97a6bfecd1ee080ec80b5dba8f0047de590
MaksMusic <Maksmusic05@yandex.ru>
2022-09-06 00:26:14 +0300

    Edit Set and List

--- src/main/kotlin/Collection/ArrayList.kt
+++ src/main/kotlin/Collection/ArrayList.kt
@@ -3,46 +3,56 @@ package Collection
 import Student
 
 fun main() {
-
+    // Изменяемые коллекции MutableIterable
     var list = ArrayList<Student>(25) // все студенты
-    list.add(Student("Jon","Lachetti",2))
-    list.add(Student("Tom","Skoletta",2))
-    list.add(Student("Katy","Lermantova",3))
-    list.add(Student("Anna","Stivina",3))
-    list.add(Student("Halk","Grandsinov",1))
-    list.add(Student("MisterX","Jesikson",1))
+    list.add(Student("Jon", "Lachetti", 2))
+    list.add(Student("Tom", "Skoletta", 2))
+    list.add(Student("Katy", "Lermantova", 3))
+    list.add(Student("Anna", "Stivina", 3))
+    list.add(Student("Halk", "Grandsinov", 1))
+    list.add(Student("MisterX", "Jesikson", 1))
 
 
     var list2 = ArrayList<Student>(25) //студенты сдавшие зачет
-    list2.add(Student("Tom","Skoletta",2))
-    list2.add(Student("Anna","Stivina",3))
-    list2.add(Student("MisterX","Jesikson",1))
+    list2.add(Student("Tom", "Skoletta", 2))
+    list2.add(Student("Anna", "Stivina", 3))
+    list2.add(Student("MisterX", "Jesikson", 1))
 
     var listPassedTheExam = ArrayList<Student>()
-        listPassedTheExam.addAll(list) // копирование листа
+    listPassedTheExam.addAll(list) // копирование листа
 
     //удалить лист из листа
     list.removeAll(list2) // список на отчисление
-    list.forEach( { println("$it  - отчисление")})
+    list.forEach({ println("$it  - отчисление") })
 
     println()
 
     // список студентов на след курс
     listPassedTheExam.retainAll(list)
-    list2.forEach({ println("$it  - переход на след курс")})
+    list2.forEach({ println("$it  - переход на след курс") })
 
 
     //поиск поступивших студентов
     //добавление объекта
-    var studentTomas = Student("Tomas","Vesikson",1)
+    var studentTomas = Student("Tomas", "Vesikson", 1)
     listPassedTheExam.add(studentTomas)
 
     //поиск объекта по ссылке
-   var search  = listPassedTheExam.contains(studentTomas)
+    var search = listPassedTheExam.contains(studentTomas)
     println(search)
 
 
-
+//  не изменяемые коллекции
+    var listTwo = listOf<Student>(
+        Student("Tomas", "Janro", 1),
+        Student("Tomas", "Janro", 1),
+        Student("Tomas", "Janro", 1),
+        Student("Tomas1", "Janro1", 2),
+        Student("Tomas11", "Janro11", 1)
+    )
+    println(listTwo.lastIndex)
+    println(listTwo.size)
+    println(listTwo.get(2).name)
 
 }
 
--- src/main/kotlin/Collection/Set.kt
+++ src/main/kotlin/Collection/Set.kt
@@ -1,11 +1,27 @@
 package Collection
 
 fun main() {
-    var setList = HashSet<String>()
+    // Изменяемые коллекции MutableIterable
+    var setList = HashSet<String>(21)
     setList.add("Joni")
     setList.add("Joni")
+    setList.add("Joni2")
     setList.add("Joni")
-    setList.add("Joni")
-    setList.add("Joni")
+    setList.add("Joni1")
+    setList.remove("Joni2")
     println(setList)
+
+    var setList2  = mutableSetOf<String>("Jino","Francisko","Enmili","Jino","Enmili")
+    println( setList2.add("Brasko"))
+    println(setList2.add("Seena"))
+    println(setList2)
+
+    // Неизменяемые коллекции
+    var setOf = setOf<String>("Joni","Tomy","Katy")
+    println(setOf.contains("Joni"))
+    println(setOf.size)
+    println(setOf.isEmpty())
+    println(setOf)
+
+
 }
