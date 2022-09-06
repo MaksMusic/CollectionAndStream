c53bdd0d753c2aeb6313562d7f98d69338a04bd3
MaksMusic <Maksmusic05@yandex.ru>
2022-09-06 17:43:30 +0300

    eddit queue

--- src/main/kotlin/Collection/Que.kt
+++ src/main/kotlin/Collection/Que.kt
@@ -1,27 +1,26 @@
 package Collection
 
 import java.util.*
-import kotlin.system.exitProcess
 
 fun main() {
-    var que = PriorityQueue<Int>()
-    que.add(7)
-    que.add(1)
-    que.add(3)
-    que.add(2)
-    que.add(5)
-    println(que.element()) //получить
-    println(que)
-    println(que.offer(11))
-    println(que)
-    println(que.remove())
-    println(que)
+    var queue = PriorityQueue<Int>()
+    queue.add(7)
+    queue.add(1)
+    queue.add(3)
+    queue.add(2)
+    queue.offer(5)
+    println(queue.element()) //получить
+    println(queue)
+    println(queue.offer(11))
+    println(queue)
+    println(queue.remove())
+    println(queue)
 
 
 var n = System.currentTimeMillis()
 
     for (i in 1..10001110){
-        que.offer(i)
+        queue.offer(i)
     }
 
     println((System.currentTimeMillis() - n).toFloat() / 1000.0)
