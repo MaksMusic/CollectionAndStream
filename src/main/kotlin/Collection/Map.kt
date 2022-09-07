2c82197e072f8e5b336c742cd6fba4018b05bd00
MaksMusic <Maksmusic05@yandex.ru>
2022-09-07 23:57:10 +0300

    add HashMap

---
+++ src/main/kotlin/Collection/Map.kt
@@ -0,0 +1,12 @@
+package Collection
+
+fun main() {
+    var acc = HashMap<String,String>()
+    acc.put("Login1","password1")
+    acc.put("Login2","password2")
+    acc.put("Login3","password3")
+    acc.put("Login4","password4")
+    acc.put("Login5","password5")
+
+    println(acc)
+}
