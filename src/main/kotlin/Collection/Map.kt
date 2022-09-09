29a67648c17537dd187c8571f10a60895cd461d2
MaksMusic <Maksmusic05@yandex.ru>
2022-09-09 23:55:52 +0300

    edit HashMap

--- src/main/kotlin/Collection/Map.kt
+++ src/main/kotlin/Collection/Map.kt
@@ -2,21 +2,32 @@ package Collection
 
 fun main() {
     // изменяемая коллекция
+    var n1 = mutableMapOf<String,String>("S1" to "123","S2" to "456" , "S3" to "7891011" )
+    var n11 = LinkedHashMap<String,String>()
+    var n2 = HashMap<String,String>()
+
+    n11.putAll(n1)
+    println(n11)
+
+    n2.putAll(n1)
+    println(n2)
+
     var acc = HashMap<String,String>()
     var acc2 = HashMap<String,String>()
-    acc2.put("qqqqq","12345")
+    acc2.put("qqqqq","12345") // добавить элемент если ключ есть заменит по ключу элемент
     acc2.put("sssss","12345")
-    acc2.put("sssss","1234578")  //если ключ есть заменит по ключу элемент
+    acc2.put("sssss","1234578")
     println(acc2)
 
     acc.put("Login1","password1")
     acc.put("Login2","password2")
     acc.put("Login3","password3")
     acc.put("Login4","password4")
-    acc.put("Login5","password5")
-    acc.putAll(acc2) //добавить другой map в конец списка
-    acc.set("Login1" , "12345")                 //заменяет элемент по ключу
-    var s:String? = acc.remove("Login41")  // удаляет и возвращаеь элемент если нет вернет null
+    acc.put("Login5","password5")                // добавить элемент если ключ есть заменит по ключу элемент
+    acc.putAll(acc2)                            //добавить другой map в конец списка
+    acc.set("Login1" , "12345")                //заменяет элемент по ключу
+    acc["Login17"] =  "12345"                 //заменяет элемент по ключу или добавит новый элемент
+    var s:String? = acc.remove("Login41") // удаляет и возвращает элемент если нет вернет null
 
     println(s)
     println(acc)
