ee90057f0124cbc58b174a912c3c466c117e49c6
MaksMusic <Maksmusic05@yandex.ru>
2022-09-10 00:19:54 +0300

    edit HashMap

--- src/main/kotlin/Collection/SequenceList.kt
+++ src/main/kotlin/Collection/SequenceList.kt
@@ -2,13 +2,25 @@ package Collection
 
 fun main() {
     var list = sequenceOf<String>("Tom","Jimm","Kia","Senneor"  )
+    println(list.joinToString())
 
-   list.asSequence().run {
-     this.forEach { println(it) }
-   }
+    var list2 = listOf<Int>(1,2,3,4,5,6,7,8,9,10)
+    var s = list2.asSequence()
+    println(s.joinToString())
 
-    list.run {
-        println(this)
-    }
-    println(list.asSequence())
+    //последовательности с 2 до 10
+    var nomber1 = 0
+    var numbers1 = generateSequence { nomber1 += 2;nomber1}
+    println(numbers1.take(5).joinToString())
+    //или
+
+    var nomber2 = 0
+    var numbers2 = generateSequence { nomber2 += 2; if(nomber2 > 10) null else nomber2}
+    println(numbers2.joinToString())
+    //или
+
+    var numbers3 = generateSequence(5) { if (it == 25) null else it+5 }
+    println(numbers3.joinToString())
+
+
 }
