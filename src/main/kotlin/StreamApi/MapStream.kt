5495b9bf9869958e1818762a148cacdd180a838e
MaksMusic <Maksmusic05@yandex.ru>
2022-09-11 23:52:18 +0300

    add mapStream

---
+++ src/main/kotlin/StreamApi/MapStream.kt
@@ -0,0 +1,7 @@
+package StreamApi
+
+fun main() {
+    // изменяет каждый элемент
+    var box1 = listOf<Int>(1,2,3,4,5,6,7,8,32,2,21,1,4,5,6,7,23,31,1,1,3,77)
+    box1.map { n -> n * 10 }.forEach { print("$it ") }
+}
