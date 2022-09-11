d0291b06fba09af9b1afd957e1dfbe3532178bfa
MaksMusic <Maksmusic05@yandex.ru>
2022-09-12 00:10:23 +0300

    add take Stream

--- src/main/kotlin/StreamApi/filterSream.kt
+++ src/main/kotlin/StreamApi/filterSream.kt
@@ -6,5 +6,12 @@ fun main() {
     var box1 = listOf<Int>(1,2,3,4,5,6,7,8,32,2,21,1,4,5,6,7,23,31,1,1,3,77)
     box1.filter { n -> n % 2 == 0}.forEach { print("$it ") }
     println()
-    box1.filter { n -> n % 2 != 0}.forEach { print("$it ") }
+    println(box1)
+
+
+    //возвращает в указанную коллекцию с элементами после фильтрации
+    var box11 = mutableListOf<Int>()
+    box1.filterTo(box11) { n -> n % 2 != 0}.forEach { print("$it  ") }
+    println()
+    println(box11)
 }
