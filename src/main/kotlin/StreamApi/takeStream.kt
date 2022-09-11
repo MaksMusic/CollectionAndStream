ce58eacb9b2a730244ef5980d1b579725a06e810
MaksMusic <Maksmusic05@yandex.ru>
2022-09-12 00:03:26 +0300

    add take Stream

--- src/main/kotlin/StreamApi/takeStream.kt
+++ src/main/kotlin/StreamApi/takeStream.kt
@@ -2,4 +2,8 @@ package StreamApi
 
 fun main() {
     var box1 = listOf<Int>(1,2,3,4,5,6,7,8,32,2,21,1,4,5,6,7,23,31,1,1,3,77)
+    box1.take(5).forEach { print("$it ") }
+
+    println()
+    println(box1)
 }
